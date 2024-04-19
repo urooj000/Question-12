f7b8e5f02a9e57cf380d9421b19b447bb75d0afa
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 21:59:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ["Junaid", "Ali", "Anas", "Zaviyar"];
+let message = "How Are You Dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ main.ts
@@ -0,0 +1,11 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[] = ["Junaid", "Ali", "Anas", "Zaviyar"]
+
+let message: string = "How Are You Dear?";
+
+console.log(names[0] + " " + message)
+console.log(names[1] + " " + message)
+console.log(names[2] + " " + message)
+console.log(names[3] + " " + message)
+
